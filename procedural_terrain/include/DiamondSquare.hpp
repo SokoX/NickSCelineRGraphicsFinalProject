f0371fa2654dedeaa1db797dbce3ef8f5521dec0
Nick Sokolović <47908620+SokoX@users.noreply.github.com>
2023-06-20 17:36:58 -0400

    Created DiamondSquare.hpp

---
+++ procedural_terrain/include/DiamondSquare.hpp
@@ -0,0 +1,30 @@
+#ifndef DIAMONDSQUARE_HPP
+#define DIAMONDSQUARE_HPP
+
+#include <vector>
+#include <string>
+
+struct DiamondSquare{
+
+    // Fields
+    vector<uint_8> pixelData;
+    vector<uint_8> colorData;
+    uint_8 max;
+    uint_8 waterLevel;
+
+    // Methods
+
+    // Generates a random terrain heightmap
+    void genRandom();
+    // Generates the corresponding color map
+    void genColorMap();
+    // Creates and saves a ppm for the height or color map
+    void save(bool mode);
+    // Does the 'Square' Step of the algorithm
+    void Square();
+    // Does the 'Diamond' step of the algorithm
+    void DIamond();
+
+};
+
+#endif
