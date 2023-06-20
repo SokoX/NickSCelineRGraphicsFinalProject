92b82722cb6de52cf4c7e7506c2f053158854a93
Nick Sokolović <47908620+SokoX@users.noreply.github.com>
2023-06-20 17:46:30 -0400

    Created DiamondSquare.cpp

---
+++ procedural_terrain/src/DiamondSquare.cpp
@@ -0,0 +1,14 @@
+#include "DiamondSquare.hpp"
+
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iostream>
+
+DiamondSquare::DiamondSquare(){
+
+};
+
+DiamondSquare::~DiamondSquare(){
+
+};
